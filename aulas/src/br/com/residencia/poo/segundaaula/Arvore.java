2c2ea9918a4837e29c872330b62ce11880799927
Priscila Durão <pd.b@yahoo.com>
2022-09-19 14:54:50 -0300

    Update Arvore.java

--- aulas/src/br/com/residencia/poo/segundaaula/Arvore.java
+++ aulas/src/br/com/residencia/poo/segundaaula/Arvore.java
@@ -2,4 +2,25 @@ package br.com.residencia.poo.segundaaula;
 
 public class Arvore {
 
+	public static void main(String[] args) {
+
+		int linha = 8, t = 1;
+
+		for (int cont = 1; cont <= 8; cont++) {
+			for (int contDois = 1; contDois <= 10; contDois++)
+				if (contDois == linha) {
+					for (int contTres = 1; contTres <= t; contTres++)
+						System.out.print('*');
+
+					t = t + 2;
+					linha--;
+
+				} else {
+					System.out.print(' ');
+
+				}
+			System.out.println();
+		}
+		System.out.println("      |_|");
+	}
 }
