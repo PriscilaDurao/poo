5a61aa5678a2a122bcd171a4d244100ed4f1b0bf
Priscila Durão <pd.b@yahoo.com>
2022-09-19 14:55:11 -0300

    Update BalancoTrimestral.java

--- balanco/src/br/com/residencia/poo/balanco/BalancoTrimestral.java
+++ balanco/src/br/com/residencia/poo/balanco/BalancoTrimestral.java
@@ -1,29 +1,50 @@
 package br.com.residencia.poo.balanco;
 
+import java.text.DecimalFormat;
 
 public class BalancoTrimestral {
-
-	public double somaTrimestral (double gastosTrimestreUm, double gastosTrimestreDois, double gastosTrimestreTres, double gastosTrimestreQuatro) {
-		return gastosTrimestreUm + gastosTrimestreDois + gastosTrimestreTres + gastosTrimestreQuatro;
-		
-	}
-	public static void main(String[] args) {
-		
-		double gastosJaneiro = 15000;
-		double gastosFevereiro = 23000;
-		double gastosMarco = 17000;
-		double gastosAbril = 0, gastosMaio = 0, gastosJunho = 0, gastosJulho = 0, gastosAgosto = 0, gastosSetembro = 0, gastosOutubro = 0, gastosNovembro = 0,  gastosDezembro = 0;
-		double gastosTrimestreUm = (gastosJaneiro + gastosFevereiro + gastosMarco);
-		double gastosTrimestreDois = (gastosAbril + gastosMaio + gastosJunho);
-		double gastosTrimestreTres = (gastosJulho + gastosAgosto + gastosSetembro);
-		double gastosTrimestreQuatro = (gastosOutubro + gastosNovembro + gastosDezembro);
-		
 	
-		System.out.println("Seu gasto trimestral é " + gastosTrimestreUm + ".");
+	private double gastosJaneiro = 15000;
+	private double gastosFevereiro = 23000;
+	private double gastosMarco = 17000;
+	private double gastosTrimestreUm = gastosJaneiro + gastosFevereiro + gastosMarco;
 
-		System.out.println("Seu saldo trimestral é ");
+	public BalancoTrimestral() {
 	}
 
+	public BalancoTrimestral(Double gastosJaneiro, Double gastosFevereiro, Double gastosMarco, Double somaTotal) {
+		this.gastosJaneiro = gastosJaneiro;
+		this.gastosFevereiro = gastosFevereiro;
+		this.gastosMarco = gastosMarco;
+		this.gastosTrimestreUm = somaTotal;
+	}
+
+	public Double soma() {
+		
+		System.out.printf("\nO saldo do balanço trimestral total é R$ %.2f", gastosTrimestreUm);
+		return gastosTrimestreUm;
+	}
+
+	public Double soma(Double gastosJaneiro) {
 	
+		System.out.printf("\nO saldo do balanço de janeiro é R$ %.2f", gastosJaneiro);
+		return gastosJaneiro;
+	}
+
+	public Double soma(Double gastosJaneiro, Double gastosFevereiro) {
+		Double gastosTrimestreUm = gastosJaneiro + gastosFevereiro;
+
+		System.out.printf("\nO saldo do balanço bimestral total é R$ %.2f", gastosTrimestreUm);
+		return gastosTrimestreUm;
+	}
+
+	public Double soma(Double gastosJaneiro, Double gastosFevereiro, Double gastosMarco) {
+		Double gastosTrimestreUm = gastosJaneiro + gastosFevereiro + gastosMarco;
+
+		System.out.printf("\nO saldo do balanço trimestral total é R$ %.2f", gastosTrimestreUm);
+
+		return gastosTrimestreUm;
 	
+
+}
 }
